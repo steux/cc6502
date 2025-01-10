e153d362b39dcd5f043db4b1e342b3fa1cfadb92
Bruno STEUX <bruno.steux@gmail.com>
2025-01-10 10:07:46 +0100

    Experimental: Optimization on (pointer >> 8) + offset expression

--- src/lib.rs
+++ src/lib.rs
@@ -1936,4 +1936,15 @@ void main() { fn2(); fn3(); }
         print!("{:?}", result);
         assert!(result.contains("LDA #>(ptr+2560)\n\tSTA main_1_x"));
     }
+
+    #[test]
+    fn const_ptr_arithm_test2() {
+        let args = sargs(1);
+        let input = "const char *ptr = \"\"; void main() { char x = (ptr >> 8) - 10; }";
+        let mut output = Vec::new();
+        compile(input.as_bytes(), &mut output, &args, simple_build).unwrap();
+        let result = str::from_utf8(&output).unwrap();
+        print!("{:?}", result);
+        assert!(result.contains("LDA #>(ptr+-2560)\n\tSTA main_1_x"));
+    }
 }
